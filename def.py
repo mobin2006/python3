e75e2378130470e6c03ee3953bd131c31cb49c2f
mobin2006 <www.mahintorabiiimobin@gmail.com>
2021-08-13 14:21:11 +0430

    cd

---
+++ def.py
@@ -0,0 +1,31 @@
+# def :
+# name :    
+#     name = input("please enter your name")
+#     # name = input("please enter your last name")
+#     # name = input("what year and month were you born?")
+#     # name = input("what's your level of education?")
+#     # name = input("do you have work experience?")
+#     # name = input("how much work experience do you have?")
+#     # if:
+        
+#     #     print("You have been accepted and you can be employed in our company
+
+# print(name)
+
+
+
+
+
+
+
+
+def names():
+    name = input("enter a name")
+    while name != "shayan":
+            print("you said the wrong name...")
+            name = input("say somebody else name ... >")
+            if name =="shayan":
+               print("yes :)")
+
+
+print(names[0])
